504024f584ab047ff0d55754d691917591b95b09
filippostz <filippositzia@gmail.com>
2023-05-30 17:43:24 +0200

    Update emailsender.py

--- samples/emailsender.py
+++ samples/emailsender.py
@@ -18,15 +18,18 @@ receivers = [''] #email receiver(s)
 
 @subscribe(entity='threat')
 def send_threat(event):
-    event = json.dumps(event['threat'])
-    mailtext = MIMEText(event)
-    subject = "EDR Alert"
+    event_complete = str(event['threat'])
+    logging.info("EVENT: %s", event)
+    threat_name = str(event['threat']['threatAttrs']['name'])
+    severity = str(event['threat']['severity'])
+    mailtext = MIMEText(event_complete)
+    subject = "EDR Alert: [" + severity + "] " + threat_name
     mailtext['From'] = sender
     mailtext['Subject'] = subject
 
     with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as mail_service:
         mail_service.login(smtp_username, smtp_password)
         mail_service.sendmail(sender, receivers, mailtext.as_string())
-        logging.info("EVENT: %s", event)
+        logging.info("EVENT: %s", "email sent!")
         
      
